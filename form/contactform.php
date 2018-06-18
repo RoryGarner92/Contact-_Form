b550ec8f22584433531e6118d384fd89ae403a17
Daniel Hayden <dan@sledro.com>
2018-06-18 16:33:11 +0100

    Fingers crossed.

--- form/contactform.php
+++ form/contactform.php
@@ -2,18 +2,22 @@
 
 if (isset($_POST['submit'])){
 
-  $name = $POST['name'];
-  $subject = $POST['subject'];
-  $mailFrom = $POST['mail'];
-  $message = $POST['message'];
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
 
+
+  echo $name;
 
   $mailTo = "dan@sledro.com";
   $headers = "From: " .$mailFrom;
   $txt = "You recieved a message from".$name."\n\n".$message;
 
   mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.html?mailsend");
-}
 
+  //Redirect now that the mail is sent. 
+  header("Location: index.html?mailsent");
+
+}
 ?>
